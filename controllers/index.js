f2cddf2ace83f42322ddd17f6ce76dfc14cbafc2
daniel <danielvarelaborba@gmail.com>
2019-11-12 23:06:55 -0300

    finalizado exercicio

--- controllers/index.js
+++ controllers/index.js
@@ -11,56 +11,89 @@
 const fs = require('fs');
 const path = require('path');
 const NodeRSA = require('node-rsa');
+const md5 = require('md5');
 
 const post = async (req, res, next) => {
     
     try {
-        
-        //*****validacoes*****
-        if (req.body.nome == null || req.body.nome == "")
-            throw new Error('Campo nome inválido!');
-        
-        if (req.body.chave == null || req.body.chave == "")
-            throw new Error('Campo chave inválido!');
-
+  
         //*****implementacao*****
         const filePath = path.join(__dirname, '..', 'files', req.file.filename);
 
         //faz leitura do arquivo
         const texto = await lerFile(filePath);
 
+        var texto_md5 = md5(texto);
+
+        EncriptyKey(texto_md5);
+
         //*****retorno*****
-        res.render('index', { title: 'Result AES', result: texto });
+        res.render('index', { title: 'Sistemas Seguros', result: "Gerado arquivo binário!" });
 
     } catch (error) {
         var texto = "Não foi possível encriptar o texto, Erro: "+error.message;
-        res.render('index', { title: 'Error AES', result: texto });
+        res.render('index', { title: 'Error Sistemas Seguros', result: texto });
     }
 
 };
 
-const EncriptyKey = async (req, res, next) => {
+const EncriptyKey = async (texto) => {
     const key = new NodeRSA();
-    const input = Buffer.from('test', 'utf8');
-    const keyData = '-----BEGIN PUBLIC KEY----- MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlJ/m4gXb4/QxCvjxvq+U ZUWMxk77Cb3dZ9BYIJg925GfiWXg1chgzsHddr2H5KlrnhK94aDIR58h0RCJUGep +GJXgIoOvAY2EdKYu5wtzzaQJTqZffrVw/Msa9pzxILqaDiI9GgLswyGYdqPRP/0 3z9pox/IhwoN/47pbmYwNqB1HHx/zNMPcQrzgdItFmSUsWLRGn/Db429j800F+Gs LdGhnSwtiiVhpIr6f+we5vmDxtaMH0GEHUiRMXgM/XLpuQQOP/C4tkvj3/hex+9C QFGgdN1agMrzY0BAayLxRmxR5vtPSwsu3+W0Idh/kE96ytbAA+G089Z/1Jo7qxYF fwIDAQAB -----END PUBLIC KEY-----';
-    key.importKey(keyData, 'pkcs8-public-pem');
-
-    const encrypted = key.encrypt(input);
-
-    var texto = "Chave encriptada! encrypted: \n"+encrypted.toString('base64');
-    res.render('index', { title: 'Gerar Chave', result: texto });
+    fs.readFile("publicKeyA.pem", {encoding: 'utf-8'}, async (err,data) => {
+        if (!err) {
+            key.importKey(data, 'pkcs8-public-pem');
+            const encrypted = key.encrypt(texto);
+            var texto_v = encrypted.toString('base64');
+            await fs.writeFile("assinatura.bin", texto_v, (err) => {
+                if(err)
+                    return console.log(err);
+                console.log("The file was saved!");
+            });
+            
+        } else {
+            console.log(err);
+        }
+    });
 }
 
 const DecriptyKey = async (req, res, next) => {
     const key = new NodeRSA();
-    const input = Buffer.from('GIBejSQIdcZZOPKF1OCBlzvXjYC1Z3Bw1OJrapWGtwmaf4WRScUF2ju2cORDRF8Fp0Y8ePzawW95WIxL7AV1BFCJ5Lh66IJ6mCxEDAbMhzAiuIGz0J1RGDtAJI6XmLbMMMJ924La5AO4BxB43JUaK1qcDPPWRkWM0nRm0g2QfmF+oeHzPJ73gs2Dv8S3LtXU6/BMLcjP9Ivg99cTxl4kXYeXLdscjIo8/Dnyg/Y4WJg6XlmgeJA7L7fyA+uBf0cwKPuMXjhjfssBKeYjeowPrlYZjg0xfDbFA0+Zmuo6bUXPOwtnQ5FpVIJOky7Nbre+eU09MZxBS7uWaE72BUVvsg==', 'base64');
-    const keyData = '-----BEGIN RSA PRIVATE KEY----- MIIEpAIBAAKCAQEAlJ/m4gXb4/QxCvjxvq+UZUWMxk77Cb3dZ9BYIJg925GfiWXg 1chgzsHddr2H5KlrnhK94aDIR58h0RCJUGep+GJXgIoOvAY2EdKYu5wtzzaQJTqZ ffrVw/Msa9pzxILqaDiI9GgLswyGYdqPRP/03z9pox/IhwoN/47pbmYwNqB1HHx/ zNMPcQrzgdItFmSUsWLRGn/Db429j800F+GsLdGhnSwtiiVhpIr6f+we5vmDxtaM H0GEHUiRMXgM/XLpuQQOP/C4tkvj3/hex+9CQFGgdN1agMrzY0BAayLxRmxR5vtP Swsu3+W0Idh/kE96ytbAA+G089Z/1Jo7qxYFfwIDAQABAoIBAQCMISOQUdPcXp1q 6gMMNgGlZmjvhQIeUjyRqBefS8tu3SNdaYgOKOpVpuXHEbYx+yczmBodxPlwxfId awj8nXDdduNnl0ODNhv+u+Aza3Kpn5lS8KhN5pVsNjfYzNRAIOsgLsW6Iut8+r8t PeO9O9NGNkTEf7AS2oEORlGDPKgSp898H04VlA9u08ES4+ctExyAW3tte4Bc7V81 3KPQd9Zbl4W3qODSefsgwlnvxh2hdx9lm2a6VkoqaWCg6O3cbkgNw/I6Ry/JJaVk tIcmONK8RAh+8H6rw/d9y3gMydWpAnFGndnjIokbJg1SUsOQl2Mo9fjC76ATguWb GHRz+W3JAoGBANeBjOj1/P0VdowQdIEMTIGCXczxB09TI2hiRQ8poKY+PhSRCbLU GN2zmDIrAq1t6MB+HctkAch8QJLUaGoqlUWfC8uazyz0fQKOjCPgcUL5Q6o650ve wDGmbX8ohlnItCpAkLBk1QvcLtOWcZjSwMIxps0FDgLi53ZUMNKulRfNAoGBALCN KlpG/77/NwV0QPCn5k+dmsUcH0BVjGRJUcw5eLjtm+O46WyS4ToSl3fNwCCqXCIA nidGn5y8Zcp5tSVG5N1JfF4+qRm1DM/EURTlvnMxssaB6aNYRfabfSYXZ6Nj8xEv vWIfHSH+FEiA1CPl3nutpRboRVaEi1GdWr/5n+57AoGBAJTlDVZnnsO9cIqVU+lV dgaE7AoM/d/wsSYNv2kaecRifMH485p6sDN9QkpnLRJmPcnBWRSNpC3Aq1zHJN01 Tq0rlhq2ey20PirmEi03hQmg7v9Oc0AYRvIG6uBQP4MJCHGo6k71W702A6Rs7U+J TWTeqUA9ndCgtQssYA8wUBKBAoGAGTI8Th/91btElfGuZun/U3NxqBnIV/0diR5x nwzArtuyOMCmB4m6vPf0R/PELKVDetLNv1sz4kF84XWXJZfFh9M5/ZVButp81pX0 4F+dW4FHO09FEbvCWFx+ctY6QzY7dUNz6rH4DWbebC5+mBTxOINioS9K3fsSivZD AkzUx0sCgYALastRMBA+e6BVK4OW3NWYCSFewKhz/ZNlr8X7aS0I+hAglrHY28SU 4VXCzNdGuf5RI57tGwCLoWSr0MutyTgFmyZyeCbOB8Yg647gG7c4ufkLfvUFoG73 ko/vMYojBEwuKcwMuX+fSd1fwXXcHUb34Jgr9S7PSRM6iclck8lC2A== -----END RSA PRIVATE KEY-----';
-    key.importKey(keyData, 'pkcs1-pem');
+    fs.readFile("privateKeyA.pem", {encoding: 'utf-8'}, async (err,data) => {
+        if (!err) {
+            key.importKey(data, 'pkcs1-pem');
+            fs.readFile("assinatura.bin", {encoding: 'utf-8'}, async (err,data) => {
+                if (!err) {
+                    const decrypted = key.decrypt(data);
+                    var texto = decrypted.toString('utf8');
+                    res.render('index', { title: 'Resultado: ', result: texto });
+                } else {
+                    console.log(err);
+                }
+            });
+        } else {
+            console.log(err);
+        }
+    });
+}
 
-    const decrypted = key.decrypt(input);
-
-    var texto = "Chave encriptada! \n decrypted: \n"+decrypted.toString('utf8');
-    res.render('index', { title: 'Gerar Chave', result: texto });
+const DecriptyKeyB = async (req, res, next) => {
+    const key = new NodeRSA();
+    fs.readFile("privateKeyB.pem", {encoding: 'utf-8'}, async (err,data) => {
+        if (!err) {
+            key.importKey(data, 'pkcs1-pem');
+            fs.readFile("assinatura.bin", {encoding: 'utf-8'}, async (err,data) => {
+                if (!err) {
+                    const decrypted = key.decrypt(data);
+                    var texto = decrypted.toString('utf8');
+                    res.render('index', { title: 'Resultado: ', result: texto });
+                } else {
+                    console.log(err);
+                }
+            });
+        } else {
+            console.log(err);
+        }
+    });
 }
 
 const getGerarChaves = async (req, res, next) => {
@@ -69,7 +102,44 @@ const getGerarChaves = async (req, res, next) => {
     var publicKey = key.exportKey('pkcs8-public-pem');
     var privateKey = key.exportKey('pkcs1-pem');
 
-    var texto = "Chave foi gerada com sucesso! \n"+publicKey+"\n"+privateKey+"\n n: "+key.keyPair.n+"\n e: "+key.keyPair.e;
+    var texto = "Chaves geradas!";
+
+    await fs.writeFile("publicKeyA.pem", publicKey, (err) => {
+
+        if(err)
+            return console.log(err);
+    
+        console.log("The file was saved!");
+    });
+
+    await fs.writeFile("privateKeyA.pem", privateKey, (err) => {
+
+        if(err)
+            return console.log(err);
+    
+        console.log("The file was saved!");
+    });
+
+    var key = new NodeRSA().generateKeyPair();
+    var publicKey = key.exportKey('pkcs8-public-pem');
+    var privateKey = key.exportKey('pkcs1-pem');
+
+    await fs.writeFile("publicKeyB.pem", publicKey, (err) => {
+
+        if(err)
+            return console.log(err);
+    
+        console.log("The file was saved!");
+    });
+
+    await fs.writeFile("privateKeyB.pem", privateKey, (err) => {
+
+        if(err)
+            return console.log(err);
+    
+        console.log("The file was saved!");
+    });
+    
     res.render('index', { title: 'Gerar Chave', result: texto });
 }
 
@@ -95,5 +165,6 @@ module.exports = {
     post:post,
     getGerarChaves:getGerarChaves,
     EncriptyKey:EncriptyKey,
-    DecriptyKey:DecriptyKey
+    DecriptyKey:DecriptyKey,
+    DecriptyKeyB:DecriptyKeyB,
 }
--- routes/index.js
+++ routes/index.js
@@ -12,13 +12,15 @@ const index_controller = require('../controllers/index');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Cript AES', result: '' });
+  res.render('index', { title: 'Sistemas Seguros', result: '' });
 });
 
 /* GET gerador de chave. */
 router.get('/gerarChave', index_controller.getGerarChaves);
 
 router.get('/DecriptyKey', index_controller.DecriptyKey);
+
+router.get('/DecriptyKeyB', index_controller.DecriptyKeyB);
 
 router.get('/EncriptyKey', index_controller.EncriptyKey);
 
